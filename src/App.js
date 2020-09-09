669b0a300a632c9f7066fc0b572e5c26a0a5d7a1
Tushar Choudhari <choudharit23@gmail.com>
2020-09-09 16:19:32 +0530

    editor

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import io from 'socket.io-client';
+import Editor from '@monaco-editor/react';
 
 function App() {
   const socket = io.connect('http://localhost:5555');
-
-  return <div className="App">Tushar</div>;
+  const [theme, setTheme] = useState('dark');
+  return (
+    <div className="App">
+      <Editor height="90vh" language="cpp" theme={theme} />
+    </div>
+  );
 }
 
 export default App;
