2414bc34abd278c25bd89a02e1ea3301b3c61176
Tushar Choudhari <choudharit23@gmail.com>
2020-09-10 21:03:39 +0530

    api check

--- server/index.js
+++ server/index.js
@@ -22,6 +22,9 @@ const app = express();
 const PORT = process.env.PORT || 5555;
 
 app.use(express.static(path.join(__dirname, '..', 'build')));
+app.get('/api', (req, res) => {
+  res.json({ message: 'OK' });
+});
 
 console.log(CPP(sampleProgram, '5'));
 
