7fe43b4020934d0fe8c602741d736ba6e730a87d
Tushar Choudhari <choudharit23@gmail.com>
2020-09-10 21:29:00 +0530

    socket check

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import io from 'socket.io-client';
 import Layout from './components/Layout/Layout';
 import './App.css';
 
 function App() {
   const socket = io.connect(process.env.PUBLIC_URL || 'http://localhost:5555');
+  useEffect(() => {
+    console.log(process.env.PUBLIC_URL);
+  }, []);
   const [editorTheme, setEditorTheme] = useState('dark');
   return (
     <div className="App">
