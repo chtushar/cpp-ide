50cd55df39a7d7a8a64ab3f126c19cb3d10cd6b3
Tushar Choudhari <choudharit23@gmail.com>
2020-09-11 14:28:48 +0530

    error handling

--- server/index.js
+++ server/index.js
@@ -7,8 +7,9 @@ const app = express();
 const PORT = process.env.PORT || 5555;
 
 app.use(express.static(path.join(__dirname, '..', 'build')));
-app.get('/api', (req, res) => {
-  res.json({ message: 'OK' });
+
+app.get('/*', (req, res) => {
+  res.redirect('/');
 });
 
 let server = app.listen(PORT, () => {
